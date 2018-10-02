f9a9fc8edb90624fa21516ab8adb8cede26f5b69
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2018-10-02 17:20:13 +0200

    Skeleton

---
+++ src/main/kotlin/net/nemerosa/ontrack/rundata/OntrackRunDataApplication.kt
@@ -0,0 +1,11 @@
+package net.nemerosa.ontrack.rundata
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OntrackRunDataApplication
+
+fun main(args: Array<String>) {
+    runApplication<OntrackRunDataApplication>(*args)
+}
